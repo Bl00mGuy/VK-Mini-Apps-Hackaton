3183d5078cd678eab83aa9d85bec1d02a9ae3e08
Pavel <padmaykin@ptsecurity.com>
2024-10-12 14:56:39 +0300

    feat: add containerization

---
+++ backend/migrations/000001_create_databases.sql
@@ -0,0 +1,19 @@
+CREATE TABLE achievements (
+                              id SERIAL PRIMARY KEY,
+                              title VARCHAR(255) NOT NULL,
+                              description TEXT,
+                              user_id INTEGER NOT NULL
+);
+
+CREATE TABLE avatars (
+                         name VARCHAR(255) PRIMARY KEY,
+                         image_url VARCHAR(255) NOT NULL,
+                         interests TEXT[]  -- Массив строк для хранения интересов
+);
+
+CREATE TABLE tasks (
+                       id SERIAL PRIMARY KEY,
+                       title VARCHAR(255) NOT NULL,
+                       description TEXT,
+                       points INTEGER NOT NULL
+);
