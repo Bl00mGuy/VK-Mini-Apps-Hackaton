0405aa3d8de217696f7c5b300516b28df0906a93
Dmitry Vasilkov <113594620+DmitryVasilkovW@users.noreply.github.com>
2024-10-12 20:05:21 +0300

    fix: relations

--- backend/migrations/000001_create_database.sql
+++ backend/migrations/000001_create_database.sql
@@ -24,19 +24,24 @@ CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     dep TEXT NOT NULL,
     lvl TEXT NOT NULL,
-    course INT NOT NULL,
+    course INTEGER NOT NULL,
     sport TEXT,
     club TEXT,
     mer TEXT,
+    avatar_id INTEGER,
     FOREIGN KEY (avatar_id) REFERENCES avatars(id)
 );
 
 CREATE TABLE users_achievements (
+    user_id INTEGER,
+    achievement_id INTEGER,
     FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (achievement_id) REFERENCES achievements(id)
 );
 
 CREATE TABLE users_tasks (
+    user_id INTEGER,
+    task_id INTEGER,
     FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (task_id) REFERENCES tasks(id)
 );
